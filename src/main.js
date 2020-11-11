41973a092d30d4a6ec15ebbc4282ec80c377b166
IzzyNety <izzy.nety@gmail.com>
2020-11-11 11:45:42 +0500

    Добавление Firebase

--- src/main.js
+++ src/main.js
@@ -8,14 +8,35 @@ import messageplugin from '@/utils/message.plugin'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 Vue.config.productionTip = false
 
 Vue.use(messageplugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBTb1ksFLm13PxoDdZC3pAZ3n7h1Gm64Co",
+  authDomain: "vue-crm-e7b17.firebaseapp.com",
+  databaseURL: "https://vue-crm-e7b17.firebaseio.com",
+  projectId: "vue-crm-e7b17",
+  storageBucket: "vue-crm-e7b17.appspot.com",
+  messagingSenderId: "853760117121",
+  appId: "1:853760117121:web:d52661c20c33fd848c0080"
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
