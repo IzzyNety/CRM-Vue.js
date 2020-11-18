f7ebf3e1262fc88a9a63ee3ae6c178f3e483c6e1
IzzyNety <izzy.nety@gmail.com>
2020-11-18 16:14:24 +0500

    Работа с API, валютами и компонентами

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter'
-import messageplugin from '@/utils/message.plugin'
+import currencyFilter from '@/filters/currency.filter'
+import messagePlugin from '@/utils/message.plugin'
+import Loader from '@/components/app/Loader'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
@@ -14,9 +16,11 @@ import 'firebase/database'
 
 Vue.config.productionTip = false
 
-Vue.use(messageplugin)
+Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
+Vue.filter('currency', currencyFilter)
+Vue.component('Loader', Loader)
 
 
 firebase.initializeApp({
